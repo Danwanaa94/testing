d222b4ca6b4c391ade06e5e48070fc893a82081c
Danwanaa94 <raufyussif04@gmail.com>
2022-05-04 13:57:37 +0000

    first commit

---
+++ js/Subtract.test.js
@@ -0,0 +1,8 @@
+const Subtract = require("./Subtract")
+
+
+test("subtract two numbers", () => {
+  
+    expect(Subtract(4, 2)
+    ).toBe(2)
+})
---
+++ js/Sum.test.js
@@ -0,0 +1,6 @@
+const Sum = require("./Sum")
+
+test("properly adding two numbers", ()=>{
+    expect(Sum(1, 2)
+    ).toBe(3)
+})
